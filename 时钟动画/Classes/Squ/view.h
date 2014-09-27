144477794f7dba68f4951a5e4a3752f749224141
ZGSmile <guang@mac-xuedeiMac.local>
2014-09-27 16:40:53 +0800

    e
    vc

---
+++ 时钟动画/Classes/Squ/view.h
@@ -0,0 +1,13 @@
+//
+//  view.h
+//  时钟动画
+//
+//  Created by mac_guang on 14-9-27.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface view : NSObject
+
+@end
